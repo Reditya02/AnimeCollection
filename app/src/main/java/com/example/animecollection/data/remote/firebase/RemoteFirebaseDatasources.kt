99c6225c390223d59081f2009c24888618f92fb3
Reditya02 <putureditya@gmail.com>
2024-01-17 13:48:56 +0700

    day 3: implement save user data to firestore

--- app/src/main/java/com/example/animecollection/data/remote/firebase/RemoteFirebaseDatasources.kt
+++ app/src/main/java/com/example/animecollection/data/remote/firebase/RemoteFirebaseDatasources.kt
@@ -2,6 +2,8 @@ package com.example.animecollection.data.remote.firebase
 
 import com.example.animecollection.core.UIState
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.catch
@@ -14,7 +16,6 @@ class RemoteFirebaseDatasources {
     fun getUid(): String = Firebase.auth.currentUser?.uid ?: ""
     fun logout() = Firebase.auth.signOut()
 
-
     fun login(email: String, password: String) = flow {
         emit(UIState.Loading())
         Firebase.auth.signInWithEmailAndPassword(email, password).await()
@@ -26,9 +27,25 @@ class RemoteFirebaseDatasources {
     fun register(username: String, email: String, password: String) = flow {
         emit(UIState.Loading())
         Firebase.auth.createUserWithEmailAndPassword(email, password).await()
+        user()?.apply {
+            setUserData(username, email, uid)
+        }
         emit(UIState.Success(true))
     }.catch {
         emit(UIState.Error(it.message))
     }.flowOn(Dispatchers.IO)
+
+    fun setUserData(username: String, email: String, id: String) {
+        FirebaseFirestore
+            .getInstance()
+            .collection("user_data")
+            .document(id)
+            .apply {
+                set(hashMapOf(
+                    "username" to username,
+                    "email" to email
+                ))
+            }
+    }
 
 }
